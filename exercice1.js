b251378a7bbd3d944f531bd01e039e024b922cf5
Angelique59212 <92031713+Angelique59212@users.noreply.github.com>
2021-11-24 16:40:50 +0100

    les strings

--- exercice1.js
+++ exercice1.js
@@ -2,15 +2,25 @@ let monTexte = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
 
 // Completer le code suivant pour afficher la longueur de la variable monTexte
 
-alert(monTexte.);
+alert(monTexte.length);
 
 
 //Ajouter du code ci dessous pour réaliser la suite de l'exercice
 
+let position = monTexte.indexOf("i");
+
+let myPosition = document.getElementById("position");
+myPosition.innerHtml = "position";
+
+let news = monTexte.toUpperCase();
+
+let maChaine = document.getElementById("chaineMaj");
+maChaine.innerHTML = "news";
 
 
-
-
-
-
+for (let i = 0; i < monTexte.length; i++){
+    if (monTexte[i] === "a"){
+        alert("lettre a trouvé");
+    }
+}
 
